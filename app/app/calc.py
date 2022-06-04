cc6f1e10cfb4517ae9e120f9d2414c073517da58
Ikenna Gabriel Ifenna <flixya4gabby@gmail.com>
2022-06-04 07:08:44 +0100

    Added black for automatic formatting

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,11 @@
 """ A simple calculation """
 
-def add(x,y):
-    """ Add two numbers """
+
+def add(x, y):
+    """Add two numbers"""
     return x + y
 
-def subtract(x,y):
-    """ Subtract two numbers """
+
+def subtract(x, y):
+    """Subtract two numbers"""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,16 +4,17 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    """ Test the calc module """
+    """Test the calc module"""
 
     def test_add_two_numbers(self):
-        """ Test adding two numbers """
-        res = calc.add(5,6)
+        """Test adding two numbers"""
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_two_numbers(self):
-        """ Test subtracting two numbers """
+        """Test subtracting two numbers"""
         res = calc.subtract(15, 10)
         self.assertEqual(res, 5)
