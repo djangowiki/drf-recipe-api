a8ab6cf38ab10bb0cf2735c3c9c645754f4a1501
Ikenna Gabriel Ifenna <flixya4gabby@gmail.com>
2022-06-08 17:36:59 +0100

    Fixed linting issue

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,7 +13,10 @@ class UserAdmin(BaseUserAdmin):
     list_display = ["email", "name"]
     fieldsets = (
         (None, {"fields": ("email", "password", "name")}),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (
+            _("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser")},
+        ),
         (_("Important Dates"), {"fields": ("last_login",)}),
     )
     readonly_fields = ["last_login"]
